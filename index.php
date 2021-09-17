1b2cc5ac24224dbfa1b547b85f99b243b3595f2f
Swapnilnagare-17 <70432814+Swapnilnagare-17@users.noreply.github.com>
2021-09-17 22:09:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>    
+    <title>Home</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand text-center" href="#">Welcome To Banking System</a>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                <a class="nav-link adv" href="index.php">Home</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div>
+    <em><h3><u>Sparks Foundation</u></h3></em>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alamy.com%2Fstock-photo%2Fsbi-logo.html&psig=AOvVaw1aVqHh7oDfgKEEgqi5Dyz6&ust=1631166004763000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKj_7Y7V7vICFQAAAAAdAAAAABAD.jpg"> 
+        
+    </div>
+    <div class="cont">
+        <form action="">
+            <br>
+            <button class="btn btn-primary btn-lg btn-block " name="View Customer" value="View Customer" formaction="customer.php">View Customers</button><br>
+            <button class="btn btn-primary btn-lg btn-block " name="Transaction History" value="Transaction History" formaction="history.php">Transaction History</button>
+        </form>
+    </div>
+    
+</body>
+</html> 
